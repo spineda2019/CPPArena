eefaca7edacbd50bd5e73c7425238c06099fd378
spineda2019 <spineda@wpi.edu>
2024-01-19 09:22:24 -0500

    add prototypes of allocater

--- include/arena.hpp
+++ include/arena.hpp
@@ -1,4 +1,21 @@
 #ifndef CPPARENA_INCLUDE_ARENA_HPP
 #define CPPARENA_INCLUDE_ARENA_HPP
 
-#endif // CPPARENA_INCLUDE_ARENA_HPP
+#include <cstddef>
+#include <optional>
+#include <vector>
+
+class Arena {
+   public:
+    Arena(std::size_t arena_size);
+    ~Arena();
+
+    std::optional<void*> aralloc(std::size_t requested_bytes);
+
+   private:
+    std::vector<unsigned char> arena_memory_;
+    std::size_t capacity_;
+    std::size_t arena_location_;
+};  // Arena
+
+#endif  // CPPARENA_INCLUDE_ARENA_HPP
